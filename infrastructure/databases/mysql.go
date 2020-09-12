ca112674b845edfb6ce3a2574edf8af2977754aa
関良嗣 <seki@threenext.com>
2020-09-12 15:09:33 +0900

    option削除

--- infrastructure/databases/mysql.go
+++ infrastructure/databases/mysql.go
@@ -23,9 +23,10 @@ func (mysql *Mysql) Open() *gorm.DB {
 	name := os.Getenv("DB_NAME")
 	user := os.Getenv("DB_USER")
 	password := os.Getenv("DB_PASS")
-	option := "charset=utf8mb4&parseTime=True&loc=Local"
+	// option := "charset=utf8mb4&parseTime=True&loc=Local"
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?%s", user, password, host, name, option)
+	// dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?%s", user, password, host, name, option)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s", user, password, host, name)
 
 	db, err := gorm.Open("mysql", dsn)
 	if err != nil {
